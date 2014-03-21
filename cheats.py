d0fa66a1515bce21a09dbb716af0911339a1f0d2
varunrau <varunrau@gmail.com>
2014-03-21 15:58:30 -0700

    moved files

--- cheats.py
+++
@@ -1,6 +0,0 @@
-
-vim.command("let my_filetype = &filetype")
-filetype = vim.eval("my_filetype")
-buffer_size = 30 #vim.command("exists(cheats_buffer_size)")
-open_command = "%dvnew %s.cheats" % (buffer_size, filetype)
-vim.command(open_command)
--- cheats.vim
+++
@@ -1,9 +0,0 @@
-if !has('python')
-    finish
-endif
-
-function! Main()
-
-    pyfile cheats.py
-
-endfunction
